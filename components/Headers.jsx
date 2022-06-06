3c2517e0a84f4d8f79c0755889665ca5b4718043
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-06-06 17:08:16 +0530

    Worked on major baseline of the code

---
+++ components/Headers.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'next/link'
+
+const categories = [{name: "react", slug: "react" },{ name: "redux", slug: "redux" },]
+
+const Headers = () => {
+    return (
+        <div className='container mx-auto px-10 mb-8'>
+            <div className="border-b w-full inline-block border-blue-400 py-8">
+                <div className="block md:float-left">
+                    <Link href="/">
+                        <span className="cursor-pointer font-bold text-4xl text-white">
+                            The Blog
+                        </span>
+                    </Link>
+                </div>
+                <div className="hidden md:float-left md:contents">
+                    {categories.map((category) => (
+                    <Link key={category.slug} href={`/category/${category.slug}`}>
+                        <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+                            {category.name}
+                        </span>
+                    </Link>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Headers
