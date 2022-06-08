38bef61931dbe9e0bcfa3c303a345b5f8a2de4e2
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-06-08 16:05:21 +0530

    ALL services made

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import moment from 'moment'
+import Link from 'next/link'
+import { getRecentPosts } from '../services'
 
-const PostWidget = () => {
+
+const PostWidget = ({categories , slug}) => {
+    const [relatedPosts,setRelatedPosts]=useState([])
+
+    useEffect(()=>{
+      if(slug){
+        getSimilarPosts(categories,slug)
+          .then((result)=> setRelatedPosts(result))
+      }
+      else{
+        getRecentPosts()
+          .then((result)=> setRelatedPosts(result))
+      }
+    },[input])
   return (
     <div>PostWidget</div>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
 import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
 import React, { useState, useEffect } from 'react'
 import { Layout } from '../components'
 
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
