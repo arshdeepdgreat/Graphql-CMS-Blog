da4c670b429493b0022237c0afaa62682992b335
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-06-08 15:41:23 +0530

    Post previews/cards made

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Headers } from './'
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
-      <>
-        <Headers />
-        {children}
-      </>
+    <>
+      <Headers />
+      {children}
+    </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import {PostCard , Categories , PostWidget} from '../components'
+import {getPosts} from '../services'
 
-const posts = [
-  { title: 'First Post', excerpt: 'This is the first post' },
-  { title: 'Second Post', excerpt: 'This is the second post' },
-];
-
-const Home: NextPage = () => {
+export default function Home({ posts }){
   return (
     <div className="container mx-auto px-10 mb-8 ">
       <Head>
@@ -17,7 +13,7 @@ const Home: NextPage = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className='col-span-1 lg:col-span-8'>
           {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -31,4 +27,10 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export async function getStaticProps() {
+  const posts= (await getPosts()) || []
+
+  return{
+    props: { posts }
+  }
+}
